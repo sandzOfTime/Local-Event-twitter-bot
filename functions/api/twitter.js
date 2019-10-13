f237af9d6ae6f2e7bc45eb4c1a3ad74f2a59abff
Anthony Sands <atsands55@gmail.com>
2019-10-13 14:56:09 -0400

    Added methods to tweet events and upload images to Twitter platform

---
+++ functions/api/twitter.js
@@ -0,0 +1,58 @@
+const Twit = require('twit');
+const image2base64 = require('image-to-base64');
+
+// Initialize twitter app
+var T = new Twit({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL: true,     // optional - requires SSL certificates to be valid.
+})
+
+
+
+module.exports.postEvents = (status, media_ids) => {
+    return new Promise((resolve, reject) => {
+        T.post('statuses/update', { status: status, media_ids: media_ids }, (err, data, response) => {
+            if (err) {
+                reject(err);
+            }
+            console.log(data)
+            resolve();
+        });
+    });
+
+}
+
+
+module.exports.uploadEventImage = (imagePath, altText) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            var b64image = await image2base64(imagePath);
+        } catch (error) {
+            console.log(error);
+        }
+
+
+        T.post('media/upload', { media_data: b64image }, (err, data, response) => {
+            if (err) {
+                console.log(err);
+            }
+
+            var mediaIdStr = data.media_id_string
+            var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+
+            T.post('media/metadata/create', meta_params, (err, data, response) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve([mediaIdStr]);
+                }
+            })
+        })
+    })
+}
+
+
