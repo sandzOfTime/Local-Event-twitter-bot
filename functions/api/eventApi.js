f221fbce35c921f77b5c3b426113691f452e5669
Anthony Sands <atsands55@gmail.com>
2019-11-18 11:30:59 -0500

    Update eventApi.js

--- functions/api/eventApi.js
+++ functions/api/eventApi.js
@@ -1,7 +1,7 @@
 const rp = require('request-promise');
 
 /**
- * 
+ * Makes a request to the Event API and returns the response
  * @param {String} url          URL of the Event API 
  * @param {String} accessToken  Authorization token used to grant access to the API
  * @return {String}             JSON response of the API
